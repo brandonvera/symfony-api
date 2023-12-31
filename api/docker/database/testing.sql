116e4784dc0faa34d23faadb1e587d603b601faf
brandonvera <vera.950b@gmail.com>
2023-12-31 15:14:28 -0400

    test funcionales de login y arreglo en docker

--- api/docker/database/testing.sql
+++ api/docker/database/testing.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE IF NOT EXISTS symfony_db_test;
-GRANT ALL PRIVILEGES ON symfony_db_test.* TO 'root' IDENTIFIED BY 'root';
+CREATE DATABASE IF NOT EXISTS symfony_db_test_test;
+GRANT ALL PRIVILEGES ON symfony_db_test_test.* TO 'root' IDENTIFIED BY 'root';
